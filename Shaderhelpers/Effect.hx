e64a24b827216106f1100d333d1f3f2eb5bd58ba
lil Monkey <82131956+McJonnycat@users.noreply.github.com>
2022-03-12 14:19:00 -0800

    Create Effect.hx

---
+++ Shaderhelpers/Effect.hx
@@ -0,0 +1,8 @@
+package Shaderhelpers; 
+
+class Effect {
+	public function setValue(shader:FlxShader, variable:String, value:Float){
+		Reflect.setProperty(Reflect.getProperty(shader, 'variable'), 'value', [value]);
+	}
+	
+}
