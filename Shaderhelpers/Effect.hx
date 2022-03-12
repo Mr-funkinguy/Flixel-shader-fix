4b0dcd882350d6faf48e7438e8d99281762e23d6
lil Monkey <82131956+McJonnycat@users.noreply.github.com>
2022-03-12 14:17:06 -0800

    deleting this

--- Shaderhelpers/Effect.hx
+++
@@ -1,8 +0,0 @@
-package Shaderhelpers; 
-//this is bbpanzu code for the effect not mine 
-class Effect {
-	public function setValue(shader:FlxShader, variable:String, value:Float){
-		Reflect.setProperty(Reflect.getProperty(shader, 'variable'), 'value', [value]);
-	}
-	
-}
