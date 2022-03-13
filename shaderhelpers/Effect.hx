5a7032f72c1c589c9b6c9ac62759d5cd5853daa5
lil Monkey <82131956+McJonnycat@users.noreply.github.com>
2022-03-12 16:23:14 -0800

    FINALLY!!!

---
+++ shaderhelpers/Effect.hx
@@ -0,0 +1,8 @@
+package shaderhelpers; 
+
+class Effect {
+	public function setValue(shader:FlxFixedShader, variable:String, value:Float){
+		Reflect.setProperty(Reflect.getProperty(shader, 'variable'), 'value', [value]);
+	}
+	
+}
