2e4fc9d677c423e18e837e306ff1107ae6ae4556
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2022-04-08 19:26:18 -0700

    Delete shaderhelpers directory

--- shaderhelpers/Effect.hx
+++
@@ -1,8 +0,0 @@
-package shaderhelpers; 
-
-class Effect {
-	public function setValue(shader:FlxFixedShader, variable:String, value:Float){
-		Reflect.setProperty(Reflect.getProperty(shader, 'variable'), 'value', [value]);
-	}
-	
-}
